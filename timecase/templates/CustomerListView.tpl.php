e7a5a84c38442a906ae238a64aaed433e8552c69
Drillenissen <logicguy.mailandcontact@gmail.com>
2025-12-12 00:17:02 +0100

    Added saldi cust number to customer list

--- timecase/templates/CustomerListView.tpl.php
+++ timecase/templates/CustomerListView.tpl.php
@@ -83,12 +83,13 @@
 				<th id="header_Id">Id<# if (page.orderBy == 'Id') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
 -->
 				<th id="header_Name"><i class="icon-reorder"></i>&nbsp; Name<# if (page.orderBy == 'Name') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
+				<th id="header_SaldiKundenr"><i class="icon-barcode"></i>&nbsp; SALDI #<# if (page.orderBy == 'SaldiKundenr') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
 				<th id="header_ContactPerson"><i class="icon-user"></i>&nbsp; Contact Person<# if (page.orderBy == 'ContactPerson') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
 				<th id="header_Tel"><i class="icon-phone"></i>&nbsp; Phone<# if (page.orderBy == 'Tel') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
 				<th id="header_StatusId"><i class="icon-info-sign"></i>&nbsp; Status<# if (page.orderBy == 'StatusId') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
 				<th id="header_TotalHours"><i class="icon-time"></i>&nbsp; Total Hours<# if (page.orderBy == 'TotalHours') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
-			<th id="header_PercentageOfTotal"><i class="icon-percent"></i>&nbsp; % of Total<# if (page.orderBy == 'PercentageOfTotal') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
-			<th id="header_ABCClass"><i class="icon-tag"></i>&nbsp; ABC Class<# if (page.orderBy == 'ABCClass') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
+				<th id="header_PercentageOfTotal"><i class="icon-percent"></i>&nbsp; % of Total<# if (page.orderBy == 'PercentageOfTotal') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
+				<th id="header_ABCClass"><i class="icon-tag"></i>&nbsp; ABC Class<# if (page.orderBy == 'ABCClass') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
 							
 <!-- UNCOMMENT TO SHOW ADDITIONAL COLUMNS
 				<th id="header_Location"><i class="icon-reorder"></i>&nbsp; Location<# if (page.orderBy == 'Location') { #> <i class='icon-arrow-<#= page.orderDesc ? 'up' : 'down' #>' /><# } #></th>
@@ -108,12 +109,13 @@
 				<td><#= _.escape(item.get('id') || '') #></td>
 -->
 				<td><#= _.escape(item.get('name') || '') #></td>
+				<td><#= _.escape(item.get('saldiKundenr') || '') #></td>
 				<td><#= _.escape(item.get('contactPerson') || '') #></td>
 				<td><#= _.escape(item.get('tel') || '') #></td>
 				<td><#= _.escape(item.get('statusDescription') || '') #></td>
 				<td><#= app.formatHours(item.get('totalHours') || 0) #></td>
-			<td><#= _.escape((parseFloat(item.get('percentageOfTotal')) || 0).toFixed(1)) #>%</td>
-			<td><span class="label <#= item.get('aBCClass') === 'A' ? 'label-success' : (item.get('aBCClass') === 'B' ? 'label-warning' : 'label-info') #>"><#= _.escape(item.get('aBCClass') || 'N/A') #></span></td>
+				<td><#= _.escape((parseFloat(item.get('percentageOfTotal')) || 0).toFixed(1)) #>%</td>
+				<td><span class="label <#= item.get('aBCClass') === 'A' ? 'label-success' : (item.get('aBCClass') === 'B' ? 'label-warning' : 'label-info') #>"><#= _.escape(item.get('aBCClass') || 'N/A') #></span></td>
 				
 <!-- UNCOMMENT TO SHOW ADDITIONAL COLUMNS
 				<td><#= _.escape(item.get('location') || '') #></td>
